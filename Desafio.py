3d533f5eecb7248aa5739f0b4ffd87ba4aa898e9
SofiatiIgor-8012 <sofiatiigor@gmail.com>
2026-04-15 17:01:28 -0300

    Add files via upload

---
+++ Desafio.py
@@ -0,0 +1,16 @@
+# Lista original
+animais = ["Leão", "Onça", "Cavalo"]
+print(f"Original: {animais}")
+
+# Exibindo a lista completa
+print(f"Lista de animais: {animais}")
+
+# Acessando o primeiro elemento (indice 0)
+print(f"Primeiro animal: {animais[0]}")
+
+# Acessando o Ultimo elemento (indice -1)
+print(f"Ultimo animal: {animais[-1]}")
+
+# ADICIONAR: insere um novo elemento no final da lista
+animais.append("Jumento")
+print(f"Após adicionar: {animais}")
